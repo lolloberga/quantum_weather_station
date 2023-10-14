effa4f7c58c4c417012b6fdeded0b15b20e2f6bf
lolloberga <lorenzobergadano@gmail.com>
2023-10-14 10:23:26 +0000

    add: finish load measures script

--- load_measure.py
+++ load_measure.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from dataset.interface.db_interface import DBInterface, MySQLUrl
 from dataset.model.measure_temporary import MeasureTemporary
+from dataset.script.download_datatest import DownloadDataset
 from config import ConfigParser
+from tqdm import tqdm
 import os
 import json
 import csv
@@ -11,15 +13,24 @@ ROOT_DIR = os.path.dirname(
     os.path.abspath(__file__)
 )
 
+MEASURES = {
+    "pm25": "mu_g/m^3",
+    "pm10": "mu_g/m^3",
+    "rh" : "%",
+    "temp": "°C",
+    "pres": "hpa",
+    "gps_lat": "deg",
+    "gps_lon": "deg"
+}
+
 def main():
     # get configurations
     cfg = ConfigParser()
-    
-    RESOURCE_PATH = os.path.join(ROOT_DIR, cfg['resouces']['path']) if cfg['resouces']['path'] is not None else os.path.join(ROOT_DIR, "resources")
-    DATASET_PATH = os.path.join(ROOT_DIR, cfg['dataset']['path'])
-    FOLDER_NAME_PREFIX = cfg['dataset']['folder_name_prefix'] if cfg['dataset']['folder_name_prefix'] is not None else 'board_'
-    SENSOR_NAME_PREFIX = cfg['dataset']['sensor_name_prefix'] if cfg['dataset']['sensor_name_prefix'] is not None else 's'
-    BOARD_CONFIG_FILE = os.path.join(RESOURCE_PATH, cfg['dataset']['board_config_name']) if cfg['dataset']['board_config_name'] is not None else os.path.join(RESOURCE_PATH, 'board.json')
+    # get constants
+    DATASET_PATH = cfg.consts['DATASET_PATH']
+    FOLDER_NAME_PREFIX = cfg.consts['FOLDER_NAME_PREFIX']
+    SENSOR_NAME_PREFIX = cfg.consts['SENSOR_NAME_PREFIX']
+    BOARD_CONFIG_FILE = cfg.consts['BOARD_CONFIG_FILE']
 
     if not os.path.exists(DATASET_PATH) or os.path.isfile(DATASET_PATH):
         print('Download path not available')
@@ -28,6 +39,9 @@ def main():
         print('Board config file not available')
         exit(1)
 
+    # download dataset
+    dd = DownloadDataset()
+    dd.download_with_map_file()
 
     # connect to db
     url = MySQLUrl(cfg['database']['db_name'], cfg['database']['user'], cfg['database']['password'], cfg['database']['url'], cfg['database']['port']).get_url()
@@ -35,12 +49,11 @@ def main():
 
     # start transaction
     with db.Session() as session:
-        for filename in os.scandir(DATASET_PATH):
+        for filename in tqdm(os.scandir(DATASET_PATH), desc=f'Uploading dataset...'):
             if not filename.is_file() and filename.name.startswith(FOLDER_NAME_PREFIX):
                 board_number = int(filename.name.split(FOLDER_NAME_PREFIX)[1])
                 board = _find_board_by_id(BOARD_CONFIG_FILE, board_number)
-                print(filename.path, filename.name, board_number)
-                print(board)
+                #print(filename.path, board_number, board)
                 if board is None:
                     continue
                 
@@ -48,28 +61,38 @@ def main():
                 for csv_file in os.scandir(filename.path):
                     if csv_file.is_file() and csv_file.name.endswith('.csv') and csv_file.name.startswith(SENSOR_NAME_PREFIX):
                         sensor_id = os.path.splitext(csv_file.name)[0].split(SENSOR_NAME_PREFIX)[1]
-                        print(sensor_id)
                         try:
                             sensor_id = int(sensor_id)
-                            _load_to_temp_table(session, csv_file.path, sensor_id, skip_first=True)
+                            # check if sensor_id is inside the board
+                            measure = _check_sensor_id_into_board(sensor_id, board)
+                            if measure is not None:
+                                # load into temporary table
+                                print(f'Upload sensor {sensor_id}({measure})')
+                                _load_to_temp_table(session, csv_file.path, sensor_id, skip_first=True)
+                                session.commit()
                         except ValueError:
                             continue
                 session.commit()
+    
+    print('Finish push into temporary measures')
 
 
 def _load_to_temp_table(session: Session, csv_file: str, sensor_id: id, skip_first:bool = True) -> None:
     with open(csv_file) as f:
         csv_reader = csv.reader(f, delimiter=',')
         line_count = 0
-        for row in csv_reader:
-            if line_count == 0 and skip_first:
-                line_count += 1
+        for row in tqdm(csv_reader, desc=f'Upload rows of sensor {sensor_id}'):
+            line_count += 1
+            if line_count == 1 and skip_first:
+                continue
             else:
                 if len(row) >= 2:
                     timestamp = row[0]
                     value = float(row[1])
                     mt = MeasureTemporary(sensorId=sensor_id, timestamp=timestamp, data=value)
                     session.add(mt)
+
+        print(f'- Loaded {line_count} rows')
 
     
 def _find_board_by_id(board_config_file: str, id: int) -> dict:
@@ -78,6 +101,13 @@ def _find_board_by_id(board_config_file: str, id: int) -> dict:
         for conf in board_confs:
             if 'board_id' in conf and conf['board_id'] == id:
                 return conf
+    return None
+
+def _check_sensor_id_into_board(sensor_id: int, board: dict) -> str:
+    for measure in MEASURES.keys():
+        if measure in board:
+            if sensor_id in board[measure]:
+                return measure
     return None
 
 if __name__ == "__main__":
