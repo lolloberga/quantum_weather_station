7f93d76aecbd995133dcec0cde5a22269c97a4ab
lorenzo <lorenzo.bergadano@intesa.it>
2023-12-16 15:11:58 +0100

    fix: change package "dataset" into "database"

--- database/model/__init__.py
+++ database/model/__init__.py
@@ -1,20 +1,20 @@
 # general
-from dataset.model.board_config import BoardConfig
-from dataset.model.logical_physical_connection import LogicalPhysicalConnection
-from dataset.model.board_experiment import BoardExperiment
-from dataset.model.board import Board
-from dataset.model.experiment import Experiment
-from dataset.model.logical_sensor import LogicalSensor
-from dataset.model.measure import Measure
-from dataset.model.measure_temporary import MeasureTemporary
-from dataset.model.param_type import ParamType
-from dataset.model.physical_sensor import PhysicalSensor
-from dataset.model.unit_of_measure import UnitOfMeasure
-from dataset.model.user import User
-from dataset.model.vendor_model import VendorModel
+from database.model.board_config import BoardConfig
+from database.model.logical_physical_connection import LogicalPhysicalConnection
+from database.model.board_experiment import BoardExperiment
+from database.model.board import Board
+from database.model.experiment import Experiment
+from database.model.logical_sensor import LogicalSensor
+from database.model.measure import Measure
+from database.model.measure_temporary import MeasureTemporary
+from database.model.param_type import ParamType
+from database.model.physical_sensor import PhysicalSensor
+from database.model.unit_of_measure import UnitOfMeasure
+from database.model.user import User
+from database.model.vendor_model import VendorModel
 
 # CTE specific
-from dataset.model.packet import Packet
-from dataset.model.packet_summary import PacketSummary
-from dataset.model.packet_connection import PacketConnection
-from dataset.model.packet_measure import PacketMeasure
+from database.model.packet import Packet
+from database.model.packet_summary import PacketSummary
+from database.model.packet_connection import PacketConnection
+from database.model.packet_measure import PacketMeasure
