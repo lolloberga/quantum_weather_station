efceb7ddee3f7a3b2323f6f2a51383b1956a0f2a
Lorenzo Bergadano <lorenzobergadano@gmail.com>
2024-01-08 22:03:23 +0100

    add: VQR default hyperparams

---
+++ model/train/hyperparams/vqr_hyperparams.py
@@ -0,0 +1,26 @@
+from model.train.base.hyperparameters import Hyperparameters
+
+VQR_defaults_hyperparams = {
+    'TRAIN_SIZE': 0.75,
+    'LEARNING_RATE': 0.01,
+    'NUM_EPOCHS': 1,
+    'BATCH_SIZE': 10,
+    'NUM_LAYERS': 4,
+    'DUPLICATE_QUBITS': False,
+    'OPTIMIZER': 'adam',
+    'CRITERION': 'rmse',
+}
+
+
+class VQR_Hyperparameters(Hyperparameters):
+
+    def __init__(self, hyperparams: dict = None, append_to_other: bool = True):
+        if hyperparams is None:
+            hyperparams = VQR_defaults_hyperparams
+        else:
+            if append_to_other:
+                default_hyperparams = VQR_defaults_hyperparams
+                for k, v in hyperparams.items():
+                    default_hyperparams[k] = v
+                hyperparams = default_hyperparams.copy()
+        super().__init__(hyperparams)
